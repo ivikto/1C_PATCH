56212f49db3498478024fba868227901bf88faa4
ivikto <i.zhabchenko@yandex.ru>
2025-04-07 13:53:51 +0300

    PathStatusReq add

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,7 +12,11 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -22,42 +26,50 @@ import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Base64;
+
 import org.springframework.stereotype.Component;
 
 @Component
 @PropertySource("classpath:application.properties")
 @SpringBootApplication
-public class Main {
+public class Main implements CommandLineRunner {
+
+    @Autowired
+    private ApplicationContext applicationContext;
 
     @Value("${login}")
     private String login;
     @Value("${pass}")
     private String pass;
     private String authInfo = login + ":" + pass;
-    private static String encodedAuth;
+    private String encodedAuth = Base64.getEncoder().encodeToString(authInfo.getBytes(StandardCharsets.UTF_8));
 
 
     private static final String refKey = "170b86b0-ff3f-11ef-ab81-bb3bb1425b1c";
     private static final String propertyKey = "44a5416b-6f3a-11ee-ab59-fdf03c13e17b";
-    private static final String newValue = "9999";
+    private static final String newValue = "6666";
 
-    @PostConstruct
-    public void init() {
-
-        this.authInfo = login + ":" + pass;
-        this.encodedAuth = Base64.getEncoder().encodeToString(authInfo.getBytes(StandardCharsets.UTF_8));
+    @Override
+    public void run(String... args) throws Exception {
+        this.encodedAuth = Base64.getEncoder().encodeToString((login + ":" + pass).getBytes(StandardCharsets.UTF_8));
     }
 
+
     public static void main(String[] args) throws IOException {
+        ApplicationContext context = SpringApplication.run(Main.class, args);
+        Main main = context.getBean(Main.class);
 
 
-        String json = getRequest();
-        patchRequest(json);
+        //String json = main.getRequest();
+        //main.patchRequest(json);
+        main.patchStatusRequest();
 
     }
 
     // Выполняем PATCH запрос к 1С
-    public static void patchRequest(String json) throws IOException {
+    public void patchRequest(String json) throws IOException {
+
+
         String documentType = URLEncoder.encode("ЗаказПокупателя", StandardCharsets.UTF_8);
         String baseUrl = "https://1c.svs-tech.pro/UNF/odata/standard.odata/Document_" +
                 documentType + "(guid'" + refKey + "')";
@@ -68,12 +80,13 @@ public class Main {
             HttpPatch httpPatch = new HttpPatch(baseUrl);
 
             // Установка заголовков
-            httpPatch.setHeader("Authorization", "Basic " + encodedAuth);
+            httpPatch.setHeader("Authorization", "Basic " + this.encodedAuth);
             httpPatch.setHeader("Content-Type", "application/json");
             httpPatch.setHeader("Accept", "application/json");
 
             // Полное тело запроса со всеми табличными частями
             String jsonInputString = "{\"ДополнительныеРеквизиты\": " + json + "}";
+            System.out.println(jsonInputString);
 
             httpPatch.setEntity(new StringEntity(jsonInputString, StandardCharsets.UTF_8));
 
@@ -95,10 +108,9 @@ public class Main {
     }
 
     // Выполняем GET запрос к 1С
-    public static String getRequest() throws IOException {
+    public String getRequest() throws IOException {
         String responseBody = "";
         String number = "НФНФ-003502";
-        encodedAuth = Base64.getEncoder().encodeToString(auth.getBytes(StandardCharsets.UTF_8));
         String documentType = URLEncoder.encode("ЗаказПокупателя", StandardCharsets.UTF_8);
         String refKey = "170b86b0-ff3f-11ef-ab81-bb3bb1425b1c";
         String baseUrl = "https://1c.svs-tech.pro/UNF/odata/standard.odata/Document_" +
@@ -109,18 +121,13 @@ public class Main {
         try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
             HttpGet httpGet = new HttpGet(baseUrl);
 
-            // Установка заголовков
-            httpGet.setHeader("Authorization", "Basic " + encodedAuth);
+            httpGet.setHeader("Authorization", "Basic " + this.encodedAuth);
             httpGet.setHeader("Content-Type", "application/json");
             httpGet.setHeader("Accept", "application/json");
-
 
             try (CloseableHttpResponse response = httpClient.execute(httpGet)) {
                 int statusCode = response.getStatusLine().getStatusCode();
                 responseBody = EntityUtils.toString(response.getEntity());
-
-                //System.out.println("Response Code: " + statusCode);
-                //System.out.println("Response Body: " + responseBody);
 
                 if (statusCode >= 200 && statusCode < 300) {
                     System.out.println("Response successful");
@@ -130,6 +137,7 @@ public class Main {
                 }
             }
         }
+
         responseBody = parse(responseBody);
 
         return responseBody;
@@ -144,6 +152,8 @@ public class Main {
         for (JsonNode value : valueArray) {
             typeArray = value;
         }
+        ArrayList<String> list = new ArrayList<>();
+
         ArrayList<JsonNode> arrayList = new ArrayList<>();
         ArrayNode additionalProps = (ArrayNode) typeArray.get("ДополнительныеРеквизиты");
 
@@ -156,13 +166,56 @@ public class Main {
                 arrayList.add(additionalProp);
             }
         }
-        ArrayList<String> list = new ArrayList<>();
-
         for (JsonNode prop : arrayList) {
             list.add(prop.toString());
-
         }
 
         return list.toString();
+
+    }
+
+
+    // Выполняем PATCH запрос к 1С
+    public void patchStatusRequest() throws IOException {
+
+        String documentType = URLEncoder.encode("ЗаказПокупателя", StandardCharsets.UTF_8);
+        String baseUrl = "https://1c.svs-tech.pro/UNF/odata/standard.odata/Document_" +
+                documentType + "(guid'" + refKey + "')";
+        baseUrl = baseUrl.replaceAll(" ", "%20").replaceAll("'", "%27");
+        System.out.println("Patch Status Request URL: " + baseUrl);
+
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            HttpPatch httpPatch = new HttpPatch(baseUrl);
+
+            // Установка заголовков
+            httpPatch.setHeader("Authorization", "Basic " + this.encodedAuth);
+            httpPatch.setHeader("Content-Type", "application/json");
+            httpPatch.setHeader("Accept", "application/json");
+
+            // Полное тело запроса со всеми табличными частями
+            String jsonInputString = "{\"Ref_Key\": \"170b86b0-ff3f-11ef-ab81-bb3bb1425b1c\"" +
+                    "\"СостояниеЗаказа\": " + "\"62f84931-5b99-11ee-ab57-d6c545800abf\"," +
+                    "\"СостояниеЗаказа_Type\": \"StandardODATA.Catalog_СостоянияЗаказНарядов\"," +
+                    "}";
+            System.out.println("Строка запроса: " + jsonInputString);
+
+            httpPatch.setEntity(new StringEntity(jsonInputString, StandardCharsets.UTF_8));
+
+            try (CloseableHttpResponse response = httpClient.execute(httpPatch)) {
+                int statusCode = response.getStatusLine().getStatusCode();
+                String responseBody = EntityUtils.toString(response.getEntity());
+                System.out.println(responseBody);
+
+                System.out.println("Response Code: " + statusCode);
+                //System.out.println("Response Body: " + responseBody);
+
+                if (statusCode >= 200 && statusCode < 300) {
+                    System.out.println("Update successful");
+                    // Проверьте в 1С, что данные обновились
+                } else {
+                    System.out.println("Update failed");
+                }
+            }
+        }
     }
 }
